88c2b3180cc6ca176df54ef7506c80a5ec27e17e
yunhao chen <w0847446@myscc.ca>
2025-02-21 23:17:31 -0500

    Created button listener to show the name in the textView

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var enterName: TextView
+    private lateinit var button: Button
+    private lateinit var showName: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+        enterName = findViewById(R.id.enterName)
+        button = findViewById(R.id.button)
+        showName = findViewById(R.id.showName)
+        //set click listener to show the name text
+        button.setOnClickListener {
+            showName.setText("Hello, ${enterName.text}")
         }
     }
 }
